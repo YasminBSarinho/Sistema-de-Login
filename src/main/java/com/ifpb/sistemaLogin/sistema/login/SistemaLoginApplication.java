c6b46795c7ff216908e9b0eb3663f3d4c3bd371d
Yasmin Sarinho <yasmiinsarinho@gmail.com>
2025-06-17 10:32:16 -0300

    sprint initializer

---
+++ src/main/java/com/ifpb/sistemaLogin/sistema/login/SistemaLoginApplication.java
@@ -0,0 +1,13 @@
+package com.ifpb.sistemaLogin.sistema.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ifpb/sistemaLogin/sistema/login/SistemaLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifpb.sistemaLogin.sistema.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
